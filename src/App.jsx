b6fc0cd58190bfa885625cc3b342beee0eb5fb97
21Kl3v1s21 <klevis25sfj@gmail.com>
2026-02-11 00:35:03 +0100

    Time to look for a ride

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import { useLocation } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+
+const App = () => {
+  const [showLogin, setShowLogin] = useState(false);
+
+  const isOwnerPath = useLocation().pathname.startsWith("/owner");
+
+  return <>{!isOwnerPath && <Navbar setShowLogin={setShowLogin} />}</>;
+};
+
+export default App;
