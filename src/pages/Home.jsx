a4b769d171f4077dec04845bf509dc48a8049259
21Kl3v1s21 <klevis25sfj@gmail.com>
2026-02-11 15:50:22 +0100

    banner and testimonials added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,15 @@
+import Banner from "../components/Banner";
 import FeaturedSection from "../components/FeaturedSection";
 import Hero from "../components/Hero";
+import Testimonial from "../components/Testimonial";
 
 const Home = () => {
   return (
     <>
       <Hero />
       <FeaturedSection />
+      <Banner />
+      <Testimonial />
     </>
   );
 };
