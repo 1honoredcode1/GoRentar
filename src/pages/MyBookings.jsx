5eeab368a799dc83d4a00920cc976afc1c7ac4eb
21Kl3v1s21 <klevis25sfj@gmail.com>
2026-02-21 21:48:31 +0100

    my bookings completeeed

--- src/pages/MyBookings.jsx
+++ src/pages/MyBookings.jsx
@@ -1,20 +1,32 @@
 import { useEffect, useState } from "react";
+import toast from "react-hot-toast";
 
-import { assets, dummyMyBookingsData } from "../assets/assets";
+import { assets } from "../assets/assets";
 
 import Title from "../components/extra/Title";
 
+import { useAppContext } from "../context/AppContext";
+
 const MyBookings = () => {
-  const currency = import.meta.env.VITE_APP_CURRENCY;
+  const { currency, axios, user } = useAppContext();
 
   const [bookings, setBookings] = useState([]);
 
   const fetchMyBookings = async () => {
-    setBookings(dummyMyBookingsData);
+    try {
+      const { data } = await axios.get("/api/bookings/user");
+      if (data.success) {
+        setBookings(data.bookings);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
   };
   useEffect(() => {
-    fetchMyBookings();
-  }, []);
+    user && fetchMyBookings();
+  }, [user]);
 
   return (
     <div className="px-6 md:px-16 lg:px-24 xl:px-32 2xl:px-48 mt-16 text-sm max-w-7xl">
